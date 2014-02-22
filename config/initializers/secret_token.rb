1a0128f7da9f3a1ac9ef1672426b5dabc93b624e
gg <1989.gaurav@gmail.com>
2014-02-22 10:30:19 -0800

    initial rails auto generated project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Navvi::Application.config.secret_token = '1897ae2e3bde1269c309a7f0be611e21e51a8f7e38c24f35653928fbdf89b97ee7edea2391d6c8d7eaa0b4d6aa260c8e33c5067c696230503db4cc4d872e448c'
